5004ff77bac9f0ad136449996b073011f163d353
ps-sanatan <saisanatan.91@gmail.com>
2024-05-29 13:56:19 +0530

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -1,6 +1,7 @@
 import sys
 import json
 
+#Recurssive method
 def recursive_items(dictionary):
     for key, value in dictionary.items():
         if type(value) is dict:
@@ -12,11 +13,11 @@ def recursive_items(dictionary):
 obj_string=input("Enter object :")   ##obj = {"x": {"y": {"z" : "a"}}}
 obj = json.loads(obj_string)
 
-user_key_string=input("Enter key: ") ## key = x/y/z
-key=user_key_string.split('/').pop()
+user_key_string=input("Enter key: ")   ## key = x/y/z
+key=user_key_string.split('/').pop()   ## splits the input and pops the last value which is the key
 
 for k, v in recursive_items(obj):
-    if(k != key):
+    if(k != key):                      ## Check for required user_key
         continue;
     print(v)   ## a
     break;
